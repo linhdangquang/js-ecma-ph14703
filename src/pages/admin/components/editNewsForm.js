d91e109b3eaf531ce738787d5a35b05caaf045fc
DLinh <nogamenolife.why@gmail.com>
2022-01-13 22:53:27 +0700

    style: style focus input

--- src/pages/admin/components/editNewsForm.js
+++ src/pages/admin/components/editNewsForm.js
@@ -17,7 +17,7 @@ const EditNews = {
                       <form enctype="multipart">
                       <div class="mb-6">
                       <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Title</label>
-                      <input type="title" id="title" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" value="${foundPost.title}" required>
+                      <input type="text" id="title" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" value="${foundPost.title}" required>
                       </div>
                       <div class="mb-6">
                       <label for="file" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Images</label>
